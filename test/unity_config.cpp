dc1d16870370a1a0684c20b0c7a386ed7a8fb2b1
Steven Murdoch <s.murdoch@ucl.ac.uk>
2026-05-07 14:44:20 +0100

    Fix on-device testing

---
+++ test/unity_config.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "unity_config.h"
+
+extern "C" {
+    void unity_output_char(int c) {
+        Serial.write((uint8_t)c);
+    }
+
+    void unity_output_flush(void) {
+        Serial.flush();
+    }
+
+    void unity_output_complete(void) {
+        // We leave this empty to prevent Serial.end() 
+        // and avoid the PermissionError(13)
+        Serial.flush();
+    }
+}
